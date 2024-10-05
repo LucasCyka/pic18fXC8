65029cf8a9dbeb48f3a8816e1f309a543e667b0b
Lucas <santoslucas347@gmail.com>
2024-10-05 09:29:33 -0300

    initial commit

---
+++ 0/main.c
@@ -0,0 +1,35 @@
+// CONFIG1L
+#pragma config PLLDIV = 5       // PLL Prescaler Selection (Divide by 5 (20 MHz input))
+#pragma config CPUDIV = OSC1_PLL2 // System Clock Postscaler (OSC1/OSC2 Src: /1, 96 MHz PLL Src: /2)
+#pragma config USBDIV = 2       // USB Clock Selection (from PLL divided by 2)
+
+// CONFIG1H
+#pragma config FOSC = HS        // Oscillator Selection (HS oscillator)
+#pragma config FCMEN = OFF      // Fail-Safe Clock Monitor Disable
+#pragma config IESO = OFF       // Internal/External Oscillator Switchover Disabled
+
+// CONFIG2L
+#pragma config PWRT = OFF       // Power-up Timer Enable bit (PWRT disabled)
+#pragma config BOR = ON         // Brown-out Reset Enable (enabled)
+#pragma config BORV = 3         // Brown-out Reset Voltage (minimum setting)
+#pragma config VREGEN = OFF     // USB Voltage Regulator Disabled (since you're not using USB)
+
+// CONFIG2H
+#pragma config WDT = OFF        // Watchdog Timer Disabled
+
+#define _XTAL_FREQ 20000000 //20mhz
+
+#include<xc.h>
+
+int main(){
+    ADCON1 = 0x0F;
+    TRISA  = 0;
+    
+    for(;;){
+        LATA   = 255;
+        __delay_ms(500);
+        LATA   = 0;
+        __delay_ms(500);
+    }
+
+}
