d1f8f893ee505ace8ea752ea76c78b09f540ef91
Lucas <santoslucas347@gmail.com>
2024-10-05 10:42:29 -0300

    Interrupts project

---
+++ interrupts/main.c
@@ -0,0 +1,28 @@
+// CONFIG1L
+#pragma config PLLDIV = 5       // PLL Prescaler Selection (Divide by 5 (20 MHz input))
+#pragma config CPUDIV = OSC1_PLL2 // System Clock Postscaler (OSC1/OSC2 Src: /1, 96 MHz PLL Src: /2)
+#pragma config USBDIV = 2       // USB Clock Selection (from PLL divided by 2)
+
+// CONFIG1H
+#pragma config FOSC = HS        // Oscillator Selection (HS oscillator)
+#pragma config FCMEN = OFF      // Fail-Safe Clock Monitor Disable
+#pragma config IESO = OFF       // Internal/External Oscillator Switchover Disabled
+
+// CONFIG2L
+#pragma config PWRT = OFF       // Power-up Timer Enable bit (PWRT disabled)
+#pragma config BOR = ON         // Brown-out Reset Enable (enabled)
+#pragma config BORV = 3         // Brown-out Reset Voltage (minimum setting)
+#pragma config VREGEN = OFF     // USB Voltage Regulator Disabled (since you're not using USB)
+
+// CONFIG2H
+#pragma config WDT = OFF        // Watchdog Timer Disabled
+
+#define _XTAL_FREQ 20000000 //20mhz
+
+#include<xc.h>
+#include <proc/pic18f4550.h>
+
+int main(){
+
+    return 0;
+}
